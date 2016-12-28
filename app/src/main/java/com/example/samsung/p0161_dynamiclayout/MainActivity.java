0fc7a8c419b3e7d7a8a8c0657e773b4528f8fa2f
btow <btow@yandex.ru>
2016-12-28 16:55:02 +0500

    Xml file with the template "layout" is deleted. Code for soznania "LinearLayout" and olyakov interface is added to the method "onCreate()".

--- app/src/main/java/com/example/samsung/p0161_dynamiclayout/MainActivity.java
+++ app/src/main/java/com/example/samsung/p0161_dynamiclayout/MainActivity.java
@@ -1,13 +1,57 @@
 package com.example.samsung.p0161_dynamiclayout;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        ViewGroup.LayoutParams linearLayoutParams = new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT);
+        setContentView(linearLayout, linearLayoutParams);
+
+        ViewGroup.LayoutParams lpView = new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        TextView textView = new TextView(this);
+        textView.setText(R.string.text_view);
+        textView.setLayoutParams(lpView);
+        linearLayout.addView(textView);
+
+        Button button = new Button(this);
+        button.setText(R.string.button);
+        linearLayout.addView(button, lpView);
+
+        LinearLayout.LayoutParams leftMarginParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        leftMarginParams.leftMargin = 50;
+
+        Button button1 = new Button(this);
+        button1.setText(R.string.button1);
+        linearLayout.addView(button1, leftMarginParams);
+
+        LinearLayout.LayoutParams rightGravityParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        rightGravityParams.gravity = Gravity.RIGHT;
+
+        Button button2 = new Button(this);
+        button2.setText(R.string.button2);
+        linearLayout.addView(button2, rightGravityParams);
+
     }
 }
